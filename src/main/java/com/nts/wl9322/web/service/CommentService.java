b929cee5a4b149051fc90557738db4c18d53cf01
wl9322 <wl9322@gmail.com>
2022-08-07 23:48:30 +0900

    [create] 댓글을 위한 서비스 추가

---
+++ src/main/java/com/nts/wl9322/web/service/CommentService.java
@@ -0,0 +1,6 @@
+package com.nts.wl9322.web.service;
+
+//댓글을 위해 필요한 서비스를 정의할 인터페이스
+public interface CommentService {
+
+}
---
+++ src/main/java/com/nts/wl9322/web/service/jdbc/JDBCCommentService.java
@@ -0,0 +1,6 @@
+package com.nts.wl9322.web.service.jdbc;
+
+//댓글을 위해 인터페이스를 상속받아 JDBC로 기능을 구현할 서비스
+public class JDBCCommentService {
+
+}
