0b6ec790e733698cc9cface4eb9b427f52e72f7e
wl9322 <wl9322@gmail.com>
2022-08-08 04:14:17 +0900

    [update] 해시태그 관련 기능 및 디자인 추가

--- src/main/webapp/static/js/hashtag.js
+++ src/main/webapp/static/js/hashtag.js
@@ -1,12 +1,14 @@
 $(document).ready(function (){
     $('.btn_add').click(function(){
-        $('.hashtag').append('<span class="hashtag-text"><input type="text" name="hashtag">\
-        <button class="btn_remove w-btn-outline w-btn-gray-outline">X</button></span>');
-
-        $('.btn_remove').on('click', function () { 
-            $(this).prev().remove();    
-            $(this).next().remove();      
-            $(this).remove();                       
-        });
+        
+        if($('.hashtag').children().length != 6){
+            $('.hashtag').append('<span class="hashtag-text"><input type="text" name="hashtag" required>\
+            <button class="btn_remove w-btn-outline w-btn-gray-outline">X</button></span>');
+            console.log($('.hashtag').children().length);
+            $('.btn_remove').on('click', function () {     
+                $(this).parent().remove();                       
+            });
+        }
+        else alert("더 이상 태그를 추가할 수 없습니다!")
     });
 })
