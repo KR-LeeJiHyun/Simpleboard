a504665faf328e1b5eca4a3e71a3cb54a0631e09
wl9322 <wl9322@gmail.com>
2022-08-08 23:41:38 +0900

    [update] 게시글 상세 내용 보이기 및 좋아요/싫어요 구현

--- src/main/java/com/nts/wl9322/web/controller/DetailContoroller.java
+++ src/main/java/com/nts/wl9322/web/controller/DetailContoroller.java
@@ -1,13 +1,44 @@
 package com.nts.wl9322.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.nts.wl9322.web.entity.Post;
+import com.nts.wl9322.web.service.PostService;
 
 @Controller
 @RequestMapping("/")
 public class DetailContoroller {
+	
+	@Autowired
+	private PostService jdbcPostService;
+	
 	@RequestMapping("detail")
-	public String detail() {
+	public String detail(Model model, int id) {
+		Post post = jdbcPostService.getPost(id);
+		String[] hashtags = post.getHashtag().split(",");
+		model.addAttribute("post", post);
+		model.addAttribute("hashtags", hashtags);
+		return "detail";
+	}
+	
+	@RequestMapping("like")
+	public String like(Model model, int id) {
+		Post post = jdbcPostService.likePost(id);
+		String[] hashtags = post.getHashtag().split(",");
+		model.addAttribute("post", post);
+		model.addAttribute("hashtags", hashtags);
+		return "detail";
+	}
+	
+	@RequestMapping("unlike")
+	public String unlike(Model model, int id) {
+		Post post = jdbcPostService.unlikePost(id);
+		String[] hashtags = post.getHashtag().split(",");
+		model.addAttribute("post", post);
+		model.addAttribute("hashtags", hashtags);
 		return "detail";
 	}
 }
